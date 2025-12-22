b4b9b78b09a381341f11d8f2929819fd352481d9
Maxim Tolpeev <tma2702@mail.ru>
2025-12-22 20:17:34 +0300

    feat: initialize project structure with base styles and favicon

---
+++ src/components/Aggregator/Aggregator.jsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header'
+
+const Aggregator = () => {
+  return (
+    <div className="aggregator">
+      <Header/>
+    </div>
+  )
+}
+
+export default Aggregator
